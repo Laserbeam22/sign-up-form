1196b4deece06f3bdcd6eb9236c476bba1b606c6
Melissa <melissa.layser22@gmail.com>
2022-12-29 15:01:44 -0800

    Add func for keyup event & uppercase matching

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ const upper = document.getElementById('rule-1');
 const lower = document.getElementById('rule-2');
 const number = document.getElementById('rule-3');
 const length = document.getElementById('rule-4');
+
+
+userInput.onkeyup = function() {
+
+    var upperCaseLetters = /[A-Z]/g;
+    if(userInput.value.match(upperCaseLetters)) {
+        upper.classList.remove('invalid');
+        upper.classList.add('valid');
+    } else {
+        upper.classList.remove('valid');
+        upper.classList.add('invalid');
+    }
+}
