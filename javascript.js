ec402ce24f21f38151070140738d7fc70989a1b6
Melissa <melissa.layser22@gmail.com>
2022-12-29 14:48:42 -0800

    Make const vars to get password input & each rule

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const userInput = document.getElementById('password');
+const upper = document.getElementById('rule-1');
+const lower = document.getElementById('rule-2');
+const number = document.getElementById('rule-3');
+const length = document.getElementById('rule-4');
